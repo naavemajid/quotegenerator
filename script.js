e22a1d35690f27a06a3a78c2f2ead12d95f4651d
Majid Naave <naavemajid2001@gmail.com>
2024-05-18 13:04:43 -0800

    finished work

--- script.js
+++ script.js
@@ -3,25 +3,32 @@ const quoteText = document.getElementById("quote");
 const authorText = document.getElementById("author");
 const twitterBtn = document.getElementById("twitter");
 const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
 
 
 let data = [];
+// Show loading
+function loading(){
+    loader.hidden = false;
+    quoteContainer.hidden = true
+}
 
+// Hide Loading 
+function complete(){
+    quoteContainer.hidden = false
+    loader.hidden = true;
+}
 // Show New Quotes
 function newQuote(){
+    loading();
     const apiQuotes = data[Math.floor(Math.random() * data.length)];
     if(quote.author === null){
         authorText.textContent = "Unknown";
     }else{
         authorText.textContent = apiQuotes.author;
     }
-    // Check Quote Length to determine styling
-    // if (quote.text.length > 50){
-    //     quoteText.classList.add('long-quote');
-    // } else {
-    //     quoteText.classList.remove('long-quote');
-    // }
     quoteText.textContent = apiQuotes.text;
+    complete();
    
     
     
@@ -32,12 +39,13 @@ function newQuote(){
 const quote = "https://type.fit/api/quotes";
 
 async function getQuotes(api){
+    loading()
     try{
         const response = await fetch(api);
-         data = await response.json();
+        data = await response.json();
 
-         newQuote();
-    }catch{
+        newQuote();
+    }catch(error){
         alert("Error fetching data")
     }
     
@@ -52,3 +60,4 @@ newQuoteBtn.addEventListener('click', newQuote);
 twitterBtn.addEventListener('click', tweetQuote);
 
 getQuotes(quote);
+
